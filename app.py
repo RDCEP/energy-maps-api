297c3529f52de730859e24c46b8cf6eb0ed66b16
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2021-06-08 13:54:49 -0500

    comments on getting region

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ def get_infrastructure(infrastructure_type):
 
 @app.route('/<region>')
 def get_region(region):
+    # return json as a dict corresponding the region passed to the url
+    # we need to figure out how to map checkbox selections on the front
+    # end app to meaningful url params that we extract here
     return region
 
 @app.route('/upload', methods=['POST'])
