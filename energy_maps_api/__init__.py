df89c1fe7f795bd23e5522ccaab01bb4d49e7b29
njmattes <matteson@obstructures.org>
2021-06-09 15:04:16 -0500

    Add simple and very incomplete HTTP error handling

--- energy_maps_api/__init__.py
+++ energy_maps_api/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask
+from flask import render_template
 from energy_maps_api.create.views import bp as create_bp
 from energy_maps_api.retrieve.views import bp as retrieve_bp
 from energy_maps_api.update.views import bp as update_bp
@@ -12,3 +13,18 @@ app.register_blueprint(create_bp)
 app.register_blueprint(retrieve_bp)
 app.register_blueprint(update_bp)
 app.register_blueprint(delete_bp)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(403)
+def not_found(error):
+    return render_template('errors/403.html'), 403
+
+
+@app.errorhandler(500)
+def not_found(error):
+    return render_template('errors/500.html'), 500
