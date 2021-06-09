9f6f4eeadad82e6ccb5fced0aa4b0f46feca12b5
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2021-06-09 10:52:29 -0500

    backward compatability for Python 2.x

--- energy_maps_api/constants.py
+++ energy_maps_api/constants.py
@@ -4,7 +4,7 @@ import os
 try:
     import configparser
 except ImportError:
-    import configparser as configparser
+    import ConfigParser as configparser
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
