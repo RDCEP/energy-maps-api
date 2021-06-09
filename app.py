725f8c57b6082bc0d97a82175a8ae3426a49e0f6
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2021-06-09 09:56:45 -0500

    fix typos

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def get_infrastructure(infrastructure_type):
 
 @app.route('/<region>')
 def get_region(region):
-    # return json as a dict corresponding the region passed to the url
+    # return json as a dict corresponding to the region passed to the url.
     # we need to figure out how to map checkbox selections on the front
-    # end app to meaningful url params that we extract here
+    # end app to meaningful url params that we extract here.
     return region
 
 @app.route('/upload', methods=['POST'])
