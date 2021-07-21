e747a685476eeef15b3e9c280c7c8be0527115f0
Benjamin Kleeman <benjamin.kleeman@gmail.com>
2021-07-21 12:42:15 -0500

    skeleton comments for ingestion

---
+++ energy_maps_api/create/mongo.py
@@ -0,0 +1,7 @@
+# ingestion script
+
+# for each file in dir, open the file
+# modify the schema 
+# upload to single massive collection
+# check to see if a geospatial index already exists, create an index if needed
+# update the index periodically
